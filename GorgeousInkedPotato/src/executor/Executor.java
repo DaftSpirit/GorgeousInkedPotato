fd12d72b03495c71c83b76ce93cef87aaa3d1a8d
Karuskrokro <Karuskrokro@10.3.4.165>
2015-05-13 15:28:26 +0200

    Windaube

--- GorgeousInkedPotato/src/executor/Executor.java
+++ GorgeousInkedPotato/src/executor/Executor.java
@@ -1,15 +1,6 @@
 package executor;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -17,41 +8,30 @@ import java.util.Arrays;
 
 public class Executor {
 
-	protected OutputStream os;
-	private ByteArrayOutputStream baos = new ByteArrayOutputStream ();
-	private DataOutputStream output = new DataOutputStream (baos);
-	
-	
 	/**
 	 * 
 	 * @authors Joris
 	 * @param args name of the command and options we want to execute
 	 */
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 		Runtime runtime = Runtime.getRuntime();
 		try {
-			Process process = runtime.exec(new String[] { "D:\\Applications\\SuperCollider-3.6.6\\sclang.exe"} );
-			OutputStream os = process.getOutputStream();
-			InputStream is = process.getInputStream();
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
-			
-			OutputStreamWriter osw = new OutputStreamWriter(os);
-			osw.write("s.boot;");
-			
-			
-			
-			String s;
-			while((s=br.readLine())!=null)
-				System.out.println(s);
-			
+			//runtime.exec(new String[] {"sclang"});
+			System.out.println("je lance le prog : sclang");
+		    Process process = runtime.exec(new String[] {"sclang"});
+		    InputStream is = process.getInputStream();
+		    InputStreamReader isr = new InputStreamReader(is);
+		    BufferedReader br = new BufferedReader(isr);
+		    String line;
+
+		    while ((line = br.readLine()) != null) {
+		    	System.out.println(line);
+		    }
 		} catch (IOException e) {
-			System.out.println("Ca a foiré");			
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 	}
 
 }
---
+++ GorgeousInkedPotato/src/executor/ExecutorWindaube.java
@@ -0,0 +1,52 @@
+package executor;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+
+public class ExecutorWindaube {
+
+	protected OutputStream os;
+	private ByteArrayOutputStream baos = new ByteArrayOutputStream ();
+	private DataOutputStream output = new DataOutputStream (baos);
+	
+	
+	/**
+	 * 
+	 * @authors Joris
+	 * @param args name of the command and options we want to execute
+	 */
+	public static void main(String[] args) {
+		
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			Process process = runtime.exec(new String[] { "D:\\Applications\\SuperCollider-3.6.6\\sclang.exe"} );
+			OutputStream os = process.getOutputStream();
+			InputStream is = process.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			
+			OutputStreamWriter osw = new OutputStreamWriter(os);
+			osw.write("s.boot;");
+			
+			
+			
+			String s;
+			while((s=br.readLine())!=null)
+				System.out.println(s);
+			
+		} catch (IOException e) {
+			System.out.println("Ca a foiré");			
+			e.printStackTrace();
+		}
+		
+	
+	}
+
+}
