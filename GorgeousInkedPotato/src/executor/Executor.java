d1b0250e407ad8c2ec54eb21d21522fc70cc7410
Karuskrokro <Karuskrokro@10.3.4.165>
2015-05-13 15:24:13 +0200

    Merge + ajout output

--- GorgeousInkedPotato/src/executor/Executor.java
+++ GorgeousInkedPotato/src/executor/Executor.java
@@ -1,10 +1,57 @@
 package executor;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Executor {
 
+	protected OutputStream os;
+	private ByteArrayOutputStream baos = new ByteArrayOutputStream ();
+	private DataOutputStream output = new DataOutputStream (baos);
+	
+	
+	/**
+	 * 
+	 * @authors Joris
+	 * @param args name of the command and options we want to execute
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("coucou ! je m'éxécute");
+		
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			Process process = runtime.exec(new String[] { "D:\\Applications\\SuperCollider-3.6.6\\sclang.exe"} );
+			OutputStream os = process.getOutputStream();
+			InputStream is = process.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			
+			OutputStreamWriter osw = new OutputStreamWriter(os);
+			osw.write("s.boot;");
+			
+			
+			
+			String s;
+			while((s=br.readLine())!=null)
+				System.out.println(s);
+			
+		} catch (IOException e) {
+			System.out.println("Ca a foir�");			
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
