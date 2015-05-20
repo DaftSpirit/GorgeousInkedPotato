7bdbd3b729bfc201853768775da77cd9313b1fbc
Daft_Spirit <joris.rat@gmail.com>
2015-05-20 11:37:34 +0200

    Source format and added disconnect method for reader

--- GorgeousInkedPotato/src/executor/Executor.java
+++ GorgeousInkedPotato/src/executor/Executor.java
@@ -5,26 +5,25 @@ import java.io.IOException;
 public class Executor {
 
 	/**
-	 * runs sclang and tries to write "s.boot;" on standard input of sclang
+	 * runs sclang and lets the user write command lines to sclang
 	 * @authors Joris
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Runtime runtime = Runtime.getRuntime();
-			System.out.println("je lance le prog : sclang");
+		System.out.println("je lance le prog : sclang");
 			
-			String[] arg = { "/bin/sh", "-c", "sclang" };
+		String[] arg = { "/bin/sh", "-c", "sclang" };
 			
-			Process process;
-			try {
-				process = runtime.exec(arg);
-				Reader reader = new Reader(process);
-				new Thread(reader).start();
-				Writer writer = new Writer(process);
-				new Thread(writer).start();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		Process process;
+		try {
+			process = runtime.exec(arg);
+			Reader reader = new Reader(process);
+			new Thread(reader).start();
+			Writer writer = new Writer(process);
+			new Thread(writer).start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
