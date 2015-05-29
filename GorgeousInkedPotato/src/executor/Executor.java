5307c2e90e09ee402a44863051dd1562c1adc4db
Daft_Spirit <joris.rat@gmail.com>
2015-05-29 10:48:27 +0200

    Server_Socket plus ou moins en place + Executor Unix (le server lance l'executor UNIX)

--- GorgeousInkedPotato/src/executor/Executor.java
+++ GorgeousInkedPotato/src/executor/Executor.java
@@ -5,33 +5,57 @@ import java.io.IOException;
 import network.*;
 
 public class Executor {
+	
+	private Server_Socket ss;
+	private Runtime runtime;
+	private Process process;
 
-	/**
-	 * runs sclang and lets the user write command lines to sclang
-	 * @authors Joris
-	 */
-	public static void main(String[] args) {
-		Runtime runtime = Runtime.getRuntime();
-		System.out.println("je lance le prog : sclang");
-			
-		String[] arg = { "/bin/sh", "-c", "sclang" };
-			
-		Process process;
-		try {
-			process = runtime.exec(arg);
-			
-			Reader reader = new Reader(process);
-			new Thread(reader).start();
-			
-			Writer writer = new Writer(process);
-			new Thread(writer).start();
-			
-			Server_Socket ss = new Server_Socket();
-			writer.setMsg(ss.getS());
-			writer.send();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public Executor(Server_Socket server_Socket) {
+		this.ss = server_Socket;
+		this.runtime = Runtime.getRuntime();
 	}
+
+	public void launchReader() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void launchWriter() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void launchSclang() throws IOException{
+		System.out.println("je lance le prog : sclang");
+		String[] arg = { "/bin/sh", "-c", "sclang" };
+		
+		this.process = this.runtime.exec(arg);
+	}
+
+	
+	
+//	public static void main(String[] args) {
+//		Runtime runtime = Runtime.getRuntime();
+//		System.out.println("je lance le prog : sclang");
+//			
+//		String[] arg = { "/bin/sh", "-c", "sclang" };
+//			
+//		Process process;
+//		try {
+//			process = runtime.exec(arg);
+//			
+//			Reader reader = new Reader(process);
+//			new Thread(reader).start();
+//			
+//			Writer writer = new Writer(process);
+//			new Thread(writer).start();
+//			
+//			Server_Socket ss = new Server_Socket();
+//			//writer.setMsg(ss.getS());
+//			//writer.send();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 }
--- GorgeousInkedPotato/src/network/Server_Socket.java
+++ GorgeousInkedPotato/src/network/Server_Socket.java
@@ -1,52 +1,79 @@
 package network;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Collection;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.WebSocketImpl;
+import org.java_websocket.framing.Framedata;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import executor.Executor;
 
 
-public class Server_Socket{
+public class Server_Socket extends WebSocketServer {
 	
-	static ServerSocket _SOCKET_;
-	public static Socket server_socket1;
+	private Executor exe;
 	
-
+	public Server_Socket() throws UnknownHostException {
+		super();
+	}
 	
-	private String s = null;
+	public Server_Socket(int port) throws UnknownHostException {
+		super(new InetSocketAddress( port ));
+	}
+	
+	public Server_Socket(InetSocketAddress address) {
+		super(address);
+	}
 
-	static {
+	@Override
+	public void onOpen(WebSocket conn, ClientHandshake handshake) {
+		
+	}
+
+	@Override
+	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+		
+	}
+
+	@Override
+	public void onMessage(WebSocket conn, String message) {
+		
+	}
+
+	@Override
+	public void onError(WebSocket conn, Exception ex) {
+		
+	}
+	
+	public void sendToAll(String text) {
+		Collection<WebSocket> connections = connections();
+		for(WebSocket w : connections){
+			w.send(text);
+		}
+	}
+	
+	public void runExe() throws IOException{
+		this.exe = new Executor(this);
+		this.exe.launchSclang();
+	}
+	
+	public static void main(String[] args){
+		WebSocketImpl.DEBUG = true;
+		int port = 8887;
 		try {
-			_SOCKET_ = new ServerSocket(6969);
-		} catch (IOException e) {
+			Server_Socket ss = new Server_Socket(port);
+			
+		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	
-	public Server_Socket() throws IOException{
-		server_socket1 = _SOCKET_.accept();
-	}
-	
-	public void disconnect() {
-		try {
-			server_socket1.close();
-			_SOCKET_.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	    
-	}
-
-	public String getS() {
-		return s;
-	}
-
-	public void setS(String s) {
-		this.s = s;
-	}
 	
 }
