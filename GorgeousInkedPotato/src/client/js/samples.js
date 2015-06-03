cecd4076a945ac1bf7cbedaf4e1929845d26d99d
Daft_Spirit <joris.rat@gmail.com>
2015-06-03 09:23:42 +0200

    Added samples of code to the client

---
+++ GorgeousInkedPotato/src/client/js/samples.js
@@ -0,0 +1,35 @@
+function displayT1S1() {
+	iframe = document.getElementsByName('samples')[0];
+	iframe.src="http://jorisrat.me/pad/app/samples/tree1/sample1.txt";
+	//iframe.style.display="block";
+}
+
+function displayT1S2() {
+	iframe = document.getElementsByName('samples')[0];
+	iframe.src="http://jorisrat.me/pad/app/samples/tree1/sample2.txt";
+	//iframe.style.display="block";
+}
+
+function displayT2S1() {
+	iframe = document.getElementsByName('samples')[0];
+	iframe.src="http://jorisrat.me/pad/app/samples/tree2/sample1.txt";
+	//iframe.style.display="block";
+}
+
+function displayT2S2() {
+	iframe = document.getElementsByName('samples')[0];
+	iframe.src="http://jorisrat.me/pad/app/samples/tree2/sample2.txt";
+	//iframe.style.display="block";
+}
+
+function displayT3S1() {
+	iframe = document.getElementsByName('samples')[0];
+	iframe.src="http://jorisrat.me/pad/app/samples/tree3/sample1.txt";
+	//iframe.style.display="block";
+}
+
+function displayT3S2() {
+	iframe = document.getElementsByName('samples')[0];
+	iframe.src="http://jorisrat.me/pad/app/samples/tree3/sample2.txt";
+	//iframe.style.display="block";
+}
