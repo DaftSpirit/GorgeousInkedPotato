c665ef839fccc88bc249d9d7b48e8dbab063ecf9
Daft_Spirit <joris.rat@gmail.com>
2015-06-05 14:31:36 +0200

    Last version of gip

--- GorgeousInkedPotato/src/client/js/samples.js
+++ GorgeousInkedPotato/src/client/js/samples.js
@@ -1,16 +1,17 @@
 var t1Displayed = false;
 var t2Displayed = false;
 var t3Displayed = false;
+var t4Displayed = false;
 
 function displayT1S1() {
 	iframe = document.getElementsByName('samples')[0];
-	iframe.src="http://jorisrat.me/pad/app/samples/tree1/sample1.txt";
+	iframe.src="http://gip.jorisrat.me/app/samples/tree1/sample1.txt";
 	//iframe.style.display="block";
 }
 
 function displayT1S2() {
 	iframe = document.getElementsByName('samples')[0];
-	iframe.src="http://jorisrat.me/pad/app/samples/tree1/sample2.txt";
+	iframe.src="http://gip.jorisrat.me/app/samples/tree1/sample2.txt";
 	//iframe.style.display="block";
 }
 
@@ -38,6 +39,18 @@ function displayT3S2() {
 	//iframe.style.display="block";
 }
 
+function displayT4S1() {
+	iframe = document.getElementsByName('samples')[0];
+	iframe.src="http://gip.jorisrat.me/app/samples/tree4/sample1.txt";
+	//iframe.style.display="block";
+}
+
+function displayT4S2() {
+	iframe = document.getElementsByName('samples')[0];
+	iframe.src="http://gip.jorisrat.me/app/samples/tree4/sample2.txt";
+	//iframe.style.display="block";
+}
+
 function toggleDisplayT1() {
 	li1 = document.getElementById('t1s1');
 	li2 = document.getElementById('t1s2');
@@ -60,8 +73,8 @@ function toggleDisplayT2() {
 	li2 = document.getElementById('t2s2');
 	input = document.getElementById('tree2');
 	if(t2Displayed == false) {
-		li1.innerHTML = '<input type="button" value="sample 1" class="file" onclick="displayT2S1();"/>';
-		li2.innerHTML = '<input type="button" value="sample 2" class="file" onclick="displayT2S2();"/>';
+		li1.innerHTML = '<input type="button" value="leads" class="file" onclick="displayT2S1();"/>';
+		li2.innerHTML = '<input type="button" value="basses" class="file" onclick="displayT2S2();"/>';
 		input.value = 'v';
 		t2Displayed = true;
 	} else {
@@ -77,8 +90,8 @@ function toggleDisplayT3() {
 	li2 = document.getElementById('t3s2');
 	input = document.getElementById('tree3');
 	if(t3Displayed == false) {
-		li1.innerHTML = '<input type="button" value="sample 1" class="file" onclick="displayT3S1();"/>';
-		li2.innerHTML = '<input type="button" value="sample 2" class="file" onclick="displayT3S2();"/>';
+		li1.innerHTML = '<input type="button" value="Decays" class="file" onclick="displayT3S1();"/>';
+		li2.innerHTML = '<input type="button" value="Reverb" class="file" onclick="displayT3S2();"/>';
 		input.value = 'v';
 		t3Displayed = true;
 	} else {
@@ -86,5 +99,22 @@ function toggleDisplayT3() {
 		li2.innerHTML = '';
 		input.value = '>';
 		t3Displayed = false;
+	}
+}
+
+function toggleDisplayT4() {
+	li1 = document.getElementById('t4s1');
+	li2 = document.getElementById('t4s2');
+	input = document.getElementById('tree4');
+	if(t4Displayed == false) {
+		li1.innerHTML = '<input type="button" value="How to : Basics" class="file" onclick="displayT4S1();"/>';
+		li2.innerHTML = '<input type="button" value="How to : Patterns" class="file" onclick="displayT4S2();"/>';
+		input.value = 'v';
+		t4Displayed = true;
+	} else {
+		li1.innerHTML = '';
+		li2.innerHTML = '';
+		input.value = '>';
+		t4Displayed = false;
 	}
 }
